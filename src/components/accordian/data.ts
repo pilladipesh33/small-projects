236b435fb2122df65e698cb3112eb3b0be360c96
pilladipesh33 <pilladipesh.pd@gmail.com>
2024-02-12 22:13:36 +0530

    structure added

---
+++ src/components/accordian/data.ts
@@ -0,0 +1,14 @@
+export const accordionData = [
+    {
+        title: "section 1",
+        content: "Irure quis aliquip sit irure nostrud veniam et incididunt dolor cupidatat sint voluptate aliqua ea. Cupidatat non non Lorem laborum sunt. In Lorem aute esse consequat consectetur enim incididunt reprehenderit culpa magna anim veniam. Est laboris eu eiusmod duis exercitation sint incididunt do amet. Id nulla esse exercitation enim sunt deserunt occaecat elit anim."
+    },
+    {
+        title: "section 2",
+        content: "Irure quis aliquip sit irure nostrud veniam et incididunt dolor cupidatat sint voluptate aliqua ea. Cupidatat non non Lorem laborum sunt. In Lorem aute esse consequat consectetur enim incididunt reprehenderit culpa magna anim veniam. Est laboris eu eiusmod duis exercitation sint incididunt do amet. Id nulla esse exercitation enim sunt deserunt occaecat elit anim."
+    },
+    {
+        title: "section 3",
+        content: "Irure quis aliquip sit irure nostrud veniam et incididunt dolor cupidatat sint voluptate aliqua ea. Cupidatat non non Lorem laborum sunt. In Lorem aute esse consequat consectetur enim incididunt reprehenderit culpa magna anim veniam. Est laboris eu eiusmod duis exercitation sint incididunt do amet. Id nulla esse exercitation enim sunt deserunt occaecat elit anim."
+    },
+]
--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
 import "./style.scss";
+import { accordionData } from "./data";
+import { ChevronDown, ChevronUp} from "lucide-react";
+
+type AccordionComponentProps = {
+  title: string,
+  content: string
+}
 
 export const Accordian = () => {
-  return <div>Accordian</div>;
+  return (
+    <main>
+      <h1 className="title">Accordian</h1>
+      <div className="container">
+        {accordionData.map((items) => (
+          <div>
+            <AccordionComponent title={items.title} content={items.content} />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+const AccordionComponent = ({title, content}: AccordionComponentProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="accordionContainer">
+      <div className="header" onClick={() => setIsOpen(!isOpen)}>
+        <h1>{title}</h1>
+        {isOpen == true ? (
+          <ChevronUp height={30} width={30} />
+          ) : (
+          <ChevronDown height={30} width={30}/>
+        )}
+      </div>
+      {isOpen && <body>{content}</body>}
+      <span className="border" />
+    </div>
+  );
 };
